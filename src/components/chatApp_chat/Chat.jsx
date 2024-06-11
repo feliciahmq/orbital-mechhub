e7ea43462f0b31edeedc809939eefa16c6a64068
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-06-11 23:17:18 +0800

    Added emoji picker and message bar below

--- src/components/chatApp_chat/Chat.jsx
+++ src/components/chatApp_chat/Chat.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import './Chat.css';
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = () => {
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const handleEmoji = (e) => {
+        setText(prev => prev + e.emoji);
+        setOpen(false);
+    };
+
   return (
     <div className='chat'>
         <div className="top">
@@ -18,7 +28,22 @@ const Chat = () => {
             </div>
         </div>
         <div className="center"></div>
-        <div className="bottom"></div>
+        <div className="bottom">
+            <div className="icons">
+                <img src="./img.png" alt="" />
+                <img src="./camera.png" alt="" />
+                <img src="./mic.png" alt="" />
+            </div>
+            <input type="text" placeholder='Type a message...' 
+                value={text} onChange={e=>setText(e.target.value)} />
+            <div className="emoji">
+                <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)} />
+                <div className="picker">
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+                </div>
+            </div>
+            <button className='sendButton'>Send</button>
+        </div>
     </div>
   )
 };
