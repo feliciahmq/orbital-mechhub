d2e16236df9550df7e719154edb3bc9453796b2c
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-06-23 14:29:34 +0800

    Add redirect to login page if no account

--- src/pages/chatapp/ChatApp.jsx
+++ src/pages/chatapp/ChatApp.jsx
@@ -4,18 +4,28 @@ import Chat from "../../components/chatApp_chat/Chat";
 import Detail from "../../components/chatApp_detail/Detail";
 import List from "../../components/chatApp_list/List";
 import Header from "../../components/header/Header";
+import LoginSignupForm from "../../pages/registration/LoginSignupForm.jsx";
 
 const ChatApp = () => {
+
+  const user = false;
+
   return (
     <div className='chat-page'>
-      <Header className='chat-header'/>
-        <div className='chat-container'>
-          <div className='inner-container'>
-            <List/>
-            <Chat/>
-            <Detail/>
-          </div>
-        </div>
+      {
+        user ? ( 
+          <>
+            <Header className='chat-header'/>
+              <div className='chat-container'>
+                <div className='inner-container'>
+                  <List/>
+                  <Chat/>
+                  <Detail/>
+                </div>
+              </div>
+          </> 
+        ) : (<LoginSignupForm/>)
+      }
     </div>
   )
 }
