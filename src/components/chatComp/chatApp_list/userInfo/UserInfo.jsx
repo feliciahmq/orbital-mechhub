69bc48580b8ae85878d3ec3a460832a36afecca4
Vanessa Lai <vlaiyz2605@gmail.com>
2024-06-26 15:10:39 +0800

    changed to 4 spacing

--- src/components/chatComp/chatApp_list/userInfo/UserInfo.jsx
+++ src/components/chatComp/chatApp_list/userInfo/UserInfo.jsx
@@ -2,18 +2,18 @@ import { useUserStore } from '../../../../lib/userStore';
 import './UserInfo.css';
 
 const UserInfo = () => {
-  const {currentUser } = useUserStore();
+    const {currentUser } = useUserStore();
 
-  return (
-    <div className='userInfo'>
-        <div className='user'>
-            <img src={currentUser.profilePic || '/chat-icons/avatar.png'} alt='' />
-            <h2>{currentUser.username}</h2>
+    return (
+        <div className='userInfo'>
+            <div className='user'>
+                <img src={currentUser.profilePic || '/chat-icons/avatar.png'} alt='' />
+                <h2>{currentUser.username}</h2>
+            </div>
+            <div className='icons'>
+            </div>
         </div>
-        <div className='icons'>
-        </div>
-    </div>
-  )
+    )
 };
 
 export default UserInfo;
