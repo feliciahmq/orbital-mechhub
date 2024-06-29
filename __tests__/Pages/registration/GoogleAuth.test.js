584d5482466f74dc6fac8055f492db7d3b1c5e49
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-06-29 09:51:08 +0800

    Unit testing setup

---
+++ __tests__/Pages/registration/GoogleAuth.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+describe('google auth', () => {
+
+    test('handles signup correctly', async () => {
+
+    });
+
+    test('handles login correctly', async () => {
+
+    });
+
+    test('renders error notifications', async () => {
+
+    });
+
+})
