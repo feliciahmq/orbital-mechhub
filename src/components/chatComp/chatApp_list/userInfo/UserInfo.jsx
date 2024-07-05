4053784fca983f3c4ebd645b73b346e21dd4c097
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-07-05 14:48:17 +0800

    Add: User profile navigate from chat

--- src/components/chatComp/chatApp_list/userInfo/UserInfo.jsx
+++ src/components/chatComp/chatApp_list/userInfo/UserInfo.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import { useUserStore } from '../../../../lib/userStore';
 import './UserInfo.css';
 
 const UserInfo = () => {
     const {currentUser } = useUserStore();
+    const navigate = useNavigate();
+
+    const handleProfileClick = () => {
+        navigate(`/profile/${currentUser.id}`);
+    }
 
     return (
         <div className='userInfo'>
-            <div className='user'>
+            <div className='user' onClick={handleProfileClick}>
                 <img src={currentUser.profilePic || '/chat-icons/avatar.png'} alt='' />
                 <h2>{currentUser.username}</h2>
             </div>
